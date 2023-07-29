b48ac4d0566bbf5569e2a7d5a9a51a73dbd38e2e
starchery <pivotsallit@gmail.com>
2023-07-29 15:55:08 -0400

    implement serialization of gpt-generated keywords

---
+++ gpt/keywords.py
@@ -0,0 +1,48 @@
+import dataclasses
+import datetime as dt
+from typing import Optional
+
+
+# representation of what GPT converts a user query to
+# these should correspond to stored img metadata from the DB
+# this can be used to get an idea for how the DB should be structured
+@dataclasses.dataclass
+class Keywords:
+    from_: dt.datetime
+    to: dt.datetime
+    location: Optional[str]
+    subject: Optional[str]
+    with_: list[str]
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "Keywords":
+        return Keywords(
+            # convert %Y-%m-%d date strs to datetime objects
+            # min date chosen for POSIX timestamp to be 0
+            from_=dt.datetime.fromisoformat(d.get("from", "1970-01-01")),
+            to=dt.datetime.fromisoformat(d.get("to", "9999-12-31")),
+            location=d.get("location"),
+            subject=d.get("subject"),
+            with_=d.get("with", []),
+        )
+
+
+# convert str from gpt to Keywords object
+# assumes each key-value pair in the str is semicolon-delineated
+# check `test_str` for an example of how this should be formatted
+def parse_keywords(keywords: str) -> Keywords:
+    parsed_keywords: dict[str, str | list[str]] = {}
+    for kv_pair in keywords.split(";"):
+        key, val = map(str.strip, kv_pair.split(":", 1))
+        if val[0] == "[":  # list? useful for the `with` attribute
+            parsed_keywords[key] = [item.strip() for item in val[1:-1].split(",")]
+        else:
+            parsed_keywords[key] = val
+    return Keywords.from_dict(parsed_keywords)
+
+
+if __name__ == "__main__":
+    import sys
+
+    test_str = "from:2023-06-01;to:2023-06-30;location:switzerland;subject:skiing;with:[snow, mountain]"
+    print(parse_keywords(sys.argv[1] if len(sys.argv) > 1 else test_str))
--- gpt/main.py
+++ gpt/main.py
@@ -1,6 +1,5 @@
 from typing import Tuple
 import openai
-import sys
 import config.credentials
 import gpt_types as types
 
@@ -32,42 +31,45 @@ starter_prompt: list[types.Message] = [
     context("Today's date is July 29, 2023."),
     *sample_interaction(
         "How many pictures do I have from 2015?",
-        "from:2015-01-01,to:2015-12-31",
+        "from:2015-01-01;to:2015-12-31",
     ),
     *sample_interaction(
         "Show me photos from my trip to Italy last summer.",
-        "from:2022-06-01,to:2022-09-01,location:Italy,with:food vacation scenery",
+        "from:2022-06-01;to:2022-09-01;location:italy;with:[food, vacation, scenery]",
     ),
     *sample_interaction(
         "Show me pictures from my wedding day where my wife and I are smiling.",
-        "subject:wedding,with:bride and groom smiling",
+        "subject:wedding;with:[bride smiling, groom smiling]",
     ),
     *sample_interaction(
         "Find those pictures from when I went skiing in the Alps last month",
-        "from:2023-06-01,to:2023-06-30,location:switzerland,subject:skiing,with:snow mountain",
+        "from:2023-06-01;to:2023-06-30;location:switzerland;subject:skiing;with:[snow, mountain]",
     ),
     *sample_interaction(
         "Show me pictures I took of the Washington Monument two years ago.",
-        "from:2022-01-01,to:2022-12-31,location:washington D.C.,subject:washington monument",
+        "from:2022-01-01;to:2022-12-31;location:washington D.C.;subject:washington monument",
     ),
 ]
 
-# expected: from:2020-01-01,to:2020-12-31,location:Australia,subject:kangaroo
-test_msg: types.Message = {
-    "role": "user",
-    "content": "Show me photos from when I saw a kangaroo in Australia three years ago.",
-}
+if __name__ == "__main__":
+    import sys
 
-# allow supplying test msgs from commandline
-if len(sys.argv) > 1:
-    test_msg["content"] = sys.argv[1]
+    # expected: from:2020-01-01,to:2020-12-31,location:Australia,subject:kangaroo
+    test_msg: types.Message = {
+        "role": "user",
+        "content": "Show me photos from when I saw a kangaroo in Australia three years ago.",
+    }
 
-# provide our example interactions + the new test msg
-response: types.ChatCompletion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=starter_prompt + [test_msg],
-    temperature=0,  # ensures responses are deterministic
-)  # type: ignore
+    # allow supplying test msgs from commandline
+    if len(sys.argv) > 1:
+        test_msg["content"] = sys.argv[1]
 
-print(response, end="\n\n\n")
-print(response["choices"][0]["message"]["content"])
+    # provide our example interactions + the new test msg
+    response: types.ChatCompletion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=starter_prompt + [test_msg],
+        temperature=0,  # ensures responses are deterministic
+    )  # type: ignore
+
+    print(response, end="\n\n\n")
+    print(response["choices"][0]["message"]["content"])
