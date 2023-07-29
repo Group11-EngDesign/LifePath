270b532cdfa5b3aa5ab5550610639719ec9a186e
starchery <pivotsallit@gmail.com>
2023-07-29 14:11:16 -0400

    moved python subsystem into separate folder

---
+++ gpt/gpt_types.py
@@ -0,0 +1,28 @@
+from typing import Any, Literal, TypedDict, NotRequired
+
+
+class Message(TypedDict):
+    role: Literal["system"] | Literal["user"] | Literal["assistant"]
+    content: str
+    name: NotRequired[str]
+
+
+class Usage(TypedDict):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+
+
+class Choice(TypedDict):
+    message: Message
+    finish_reason: str
+    index: int
+
+
+class ChatCompletion(TypedDict):
+    id: str
+    object: str
+    created: int
+    model: str
+    usage: Usage
+    choices: list[Choice]
