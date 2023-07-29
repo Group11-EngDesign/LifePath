3b3977cce9c86e82014735fadbfeab0584c12936
starchery <pivotsallit@gmail.com>
2023-07-29 12:01:41 -0400

    initial prompt

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-import requests
+from typing import Any
 import openai
 from config import credentials
 
 
-URL = "https://api.openai.com/v1/chat/completions"
+openai.api_key = credentials.GPT3_API_KEY
 
-payload = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": f"How many pictures do I have from 2015?"}],
-    "temperature" : 1.0,
-    "top_p":1.0,
-    "n" : 1,
-    "stream": True,
-    "presence_penalty":0,
-    "frequency_penalty":0,
-}
+example_prompt = [
+    # context
+    { "role": "system", "content": "You extract relevant keywords in a machine-readable format that can be used to identify photos by their metadata."},
+    { "role": "system", "content": "If a given date is nonspecific, choose the widest range possible. For example, if they say '2015', assume that means 'from 2015-01-01 to 2015-12-31.'"},
+    { "role": "system", "content": "Today's date is July 29, 2023."},  # todo: update this and the examples below to use `datetime`
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {credentials.GPT3_API_KEY}"
-}
+    # example interactions
+    { "role": "system", "name": "example_user", "content": "How many pictures do I have from 2015?"},
+    { "role": "system", "name": "example_assistant", "content": 'from:2015-01-01,to:2015-12-31'},
+    { "role": "system", "name": "example_user", "content": "Show me photos from my trip to Italy last summer."},
+    { "role": "system", "name": "example_assistant", "content": "from:2022-06-01,to:2022-09-01,location:Italy,with:food vacation scenery"},
+    { "role": "system", "name": "example_user", "content": "Show me pictures from my wedding day where my wife and I are smiling."},
+    { "role": "system", "name": "example_assistant", "content": "subject:wedding,with:bride and groom smiling"},
+    { "role": "system", "name": "example_user", "content": "Find those pictures from when I went skiing in the Alps last month"},
+    { "role": "system", "name": "example_assistant", "content": "from:2023-06-01,to:2023-06-30,location:switzerland,subject:skiing,with:snow mountain"},
+    { "role": "system", "name": "example_user", "content": "Show me pictures I took of the Washington Monument two years ago."},
+    { "role": "system", "name": "example_assistant", "content": "from:2022-01-01,to:2022-12-31,location:washington D.C.,subject:washington monument"},
+]
 
-response = requests.post(URL, headers=headers, json=payload, stream=True)
+test_msg = { "role": "user", "content": "Show me photos from when I saw a kangaroo in Australia three years ago." }
 
-print(response.content)
+response: Any = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=example_prompt + [test_msg],
+    temperature=0
+)
+
+print(response, end='\n\n\n')
+print(response["choices"][0]["message"]["content"])
