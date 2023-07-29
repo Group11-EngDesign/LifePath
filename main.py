9db2698e1440951f6fc82c42477336f7c2c2feb8
starchery <pivotsallit@gmail.com>
2023-07-29 08:18:35 -0400

    moved api key out of committed source

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import requests
+from config import credentials
 
 
 URL = "https://api.openai.com/v1/chat/completions"
 
 payload = {
-"model": "gpt-3.5-turbo",
-"messages": [{"role": "user", "content": f"How many pictures do I have from 2015?"}],
-"temperature" : 1.0,
-"top_p":1.0,
-"n" : 1,
-"stream": True, 
-"presence_penalty":0,
-"frequency_penalty":0,
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": f"How many pictures do I have from 2015?"}],
+    "temperature" : 1.0,
+    "top_p":1.0,
+    "n" : 1,
+    "stream": True,
+    "presence_penalty":0,
+    "frequency_penalty":0,
 }
-
-api_key = "skQTdSMQ0XBdJz4Y3PVq8yT3BlbkFJqqyWnSH3iztkH8bF8Vhs"
 
 headers = {
 "Content-Type": "application/json",
-"Authorization": f"Bearer {api_key}"
+"Authorization": f"Bearer {credentials.GPT3_API_KEY}"
 }
 
 response = requests.post(URL, headers=headers, json=payload, stream=True)
-    
 
 print(response.content)
