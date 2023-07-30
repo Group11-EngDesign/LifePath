0abaec52046ed099656bdfd04d8468e11e8eda5f
jaydess21 <98706895+jaydess21@users.noreply.github.com>
2023-07-29 23:20:42 -0400

    Added search bar to the top

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, StatusBar, StyleSheet, Image } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to LifePath!</Text>
+      
+
+      <Image source={require('./assets/lifepathlogo.png')} style={styles.logo} />
+      <View style={styles.searchBarContainer}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search..."
+          placeholderTextColor="gray"
+        />
+      </View>
+
+      {/* Add the rest of your content here */}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+  },
+  searchBarContainer: {
+    width: '100%',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    marginBottom: 16,
+  },
+  searchBar: {
+    fontSize: 16,
+    color: 'black',
   },
 });
