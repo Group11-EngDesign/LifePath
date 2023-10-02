a52b455c0901e2e61ecf63f7130f5ad2ee334681
jzjr94 <102771670+jzjr94@users.noreply.github.com>
2023-10-01 21:34:32 -0400

    updated photodetails page
    added comment bar, removable comments on photodetails page

--- screens/home.js
+++ screens/home.js
@@ -1,34 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StatusBar, StyleSheet, Image, Button, ActivityIndicator, TouchableOpacity } from 'react-native';
-import { starterPrompt, parseKeywords } from '../prompt';
+import { View, Text, TextInput, StatusBar, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import axios from 'axios';
-import { getList } from '../api/picsum'
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { GPT3_API_KEY } from '../env';
 
 const Home = ({ navigation }) => {
-    const [query, setQuery] = useState(""); // Define query state
-    const [reply, setReply] = useState(""); // Define reply state
-    const [smallImageUrl, setSmallImageUrl] = useState(""); // Define state for small image URL
-    const route = useRoute();
-    const photos = route.params ? route.params.photos : []; // Get photos data from params
+  const [query, setQuery] = useState(""); // Define query state
+  const [reply, setReply] = useState(""); // Define reply state
+  const [smallImageUrl, setSmallImageUrl] = useState(""); // Define state for small image URL
+  const route = useRoute();
+  const photos = route.params ? route.params.photos : []; // Get photos data from params
 
-  
-    const handleOpenPhotoGallery = () => {
-      navigation.navigate('PhotoGallery');
-    };
-  
-    const axiosInstance = axios.create({
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${GPT3_API_KEY}`
-      }
-    });
-  
-    const processQuery = () => {
-      console.log(query);
+  const handleOpenPhotoGallery = () => {
+    navigation.navigate('PhotoGallery');
+  };
 
+  const axiosInstance = axios.create({
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${GPT3_API_KEY}`
+    }
+  });
+
+  const processQuery = () => {
+    console.log(query);
 
     // Search for the keyword in photo metadata
     const keyword = query.toLowerCase();
@@ -45,7 +41,6 @@ const Home = ({ navigation }) => {
       setSmallImageUrl("");
     }
 
-
     // Send the query to your API
     axiosInstance.post("http://192.168.0.115:8000/hello/", query)
       .then(response => response.data)
@@ -55,86 +50,92 @@ const Home = ({ navigation }) => {
       })
       .catch(err => console.error(err.message));
   };
-  
-    return (
-      <View style={styles.container}>
-        <Image source={require('../assets/lifepathlogo.png')} style={styles.logo} />
-        <Text>Your Life Memories</Text>
-        <Text>Right at Your Fingertips</Text>
-  
-        <View style={styles.searchBarContainer}>
-          <TextInput
-            style={styles.searchBar}
-            defaultValue=''
-            placeholder="Ex. Find pictures from 2016..."
-            value={query}
-            onChangeText={setQuery}
-          />
-        </View>
 
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/lifepathlogo.png')} style={styles.logo} />
+      <Text style={styles.title}>Your Life Memories</Text>
+      <Text style={styles.subtitle}>Right at Your Fingertips</Text>
 
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={processQuery} style={styles.searchButton}>
-            <Text style={styles.buttonText}>SEARCH</Text>
-          </TouchableOpacity>
-          
-
-          <TouchableOpacity style={styles.searchButton} onPress={handleOpenPhotoGallery}>
-            <Text style={styles.buttonText}>GALLERY</Text>
-          </TouchableOpacity>
-        </View>
-
-        <Text>{reply}</Text>
+      <View style={styles.searchBarContainer}>
+        <TextInput
+          style={styles.searchBar}
+          defaultValue=''
+          placeholder="Ex. Find pictures from 2016..."
+          value={query}
+          onChangeText={setQuery}
+        />
       </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-      padding: 30,
-      backgroundColor: '#FFFFFF', 
-    },
-    logo: {
-      marginBottom: 5,
-    },
-    searchBarContainer: {
-      width: '80%',
-      backgroundColor: '#f0f0f0',
-      borderRadius: 8,
-      paddingHorizontal: 12,
-      paddingVertical: 6,
-      marginBottom: 20,
-      marginTop: 20,
-      borderWidth: 1,
-      borderColor: '#7bb956',
-    },
-    searchBar: {
-      fontSize: 16,
-      color: '#7bb956',
-      padding: 3,
-    },
-    smallImage: {
-      width: 100, // Adjust the dimensions as needed
-      height: 100,
-      marginBottom: 16,
-    },
-    buttonText: {
-      color: 'white',
-    },
-    searchButton: {
-      backgroundColor: '#7bb956',
-      margin: 10,
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-    },
-    buttonContainer: {
-      flexDirection: 'row',
-    },
-  });
-  
-  export default Home;
+
+      <StatusBar style="auto" />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={processQuery} style={styles.searchButton}>
+          <Text style={styles.buttonText}>SEARCH</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.searchButton} onPress={handleOpenPhotoGallery}>
+          <Text style={styles.buttonText}>GALLERY</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text>{reply}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 30,
+    backgroundColor: '#FFFFFF',
+  },
+  logo: {
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+    fontFamily: 'Arial',
+  },
+  subtitle: {
+    fontSize: 20,
+    color: '#7bb956',
+    marginBottom: 40,
+    fontFamily: 'Arial',
+  },
+  searchBarContainer: {
+    width: '80%',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: '#7bb956',
+  },
+  searchBar: {
+    fontSize: 16,
+    color: '#7bb956',
+    padding: 3,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  searchButton: {
+    backgroundColor: '#7bb956',
+    margin: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+});
+
+export default Home;
--- screens/photodetails.js
+++ screens/photodetails.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 const PhotoDetails = ({ route }) => {
   const { photo } = route.params;
+  const [comment, setComment] = useState(''); // State to store user comments
+  const [comments, setComments] = useState([]); // State to store all comments
 
-  console.log('Photo:', photo); //checking if photo being passed
+  // Function to handle adding a comment
+  const handleAddComment = () => {
+    if (comment.trim() !== '') {
+      setComments([...comments, comment]); // Add the comment to the comments list
+      setComment(''); // Clear the input field
+    }
+  };
+
+  // Function to handle removing a comment
+  const handleRemoveComment = (index) => {
+    const updatedComments = [...comments];
+    updatedComments.splice(index, 1); // Remove the comment at the specified index
+    setComments(updatedComments);
+  };
+
+  console.log('Photo:', photo); // Checking if photo is being passed
 
   return (
     <View style={styles.container}>
@@ -17,6 +36,26 @@ const PhotoDetails = ({ route }) => {
       <Text>{photo.author}</Text>
       <Text>{photo.width} x {photo.height}</Text>
 
+      <ScrollView style={styles.commentsContainer}>
+  {comments.map((commentText, index) => (
+    <View key={index} style={styles.commentContainer}>
+      <Text style={styles.commentText}>{commentText}</Text>
+      <TouchableOpacity onPress={() => handleRemoveComment(index)} style={styles.removeButton}>
+        <Icon name="trash-o" size={18} color="" />
+      </TouchableOpacity>
+    </View>
+  ))}
+</ScrollView>
+
+      <TextInput
+        style={styles.commentInput}
+        placeholder="Add a comment..."
+        value={comment}
+        onChangeText={(text) => setComment(text)}
+      />
+      <TouchableOpacity style={styles.commentButton} onPress={handleAddComment}>
+        <Text style={styles.commentButtonText}>Add Comment</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -31,6 +70,48 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 400,
     margin: 5,
+  },
+  commentsContainer: {
+    maxHeight: 150,
+    width: '100%',
+  },
+  commentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    padding: 8,
+  },
+  commentText: {
+    flex: 1,
+  },
+  removeButton: {
+    marginLeft: 10,
+    padding: 5,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  },
+  removeButtonText: {
+    color: 'white',
+  },
+  commentInput: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 8,
+    marginTop: 10,
+  },
+  commentButton: {
+    marginTop: 10,
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  commentButtonText: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
 
